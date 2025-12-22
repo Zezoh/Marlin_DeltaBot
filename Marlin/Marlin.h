dcc58556bd0a5961f3f3a87ddc147206a7ede689
Halawania <abdu.halwani@gmail.com>
2025-12-22 04:44:33 +0300

    Revert "Guard DELTA_FAST_SQRT for delta kinematics"

--- Marlin/Marlin.h
+++ Marlin/Marlin.h
@@ -352,10 +352,6 @@ void home_all_axes();
 
 void report_current_position();
 
-#if ENABLED(DELTA) && ENABLED(DELTA_FAST_SQRT)
-  float Q_rsqrt(float number);
-#endif
-
 #if IS_KINEMATIC
   #if ENABLED(HANGPRINTER)
     extern float line_lengths[ABCD];
@@ -380,6 +376,10 @@ void report_current_position();
 
   void recalc_delta_settings();
   float delta_safe_distance_from_top();
+
+  #if ENABLED(DELTA_FAST_SQRT)
+    static float Q_rsqrt(float number);
+  #endif
 
   // Macro to obtain the Z position of an individual tower
   #define DELTA_Z(V,T) V[Z_AXIS] + SQRT(    \
