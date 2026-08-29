9a4b40a28fb3edead8a7f353b98be5397891a8b6
Halawania <abdu.halwani@gmail.com>
2026-08-29 23:47:48 +0300

    DeltaCore v0.5.3: make path planner a true ring

--- DeltaCore/src/PathPlanner.h
+++ DeltaCore/src/PathPlanner.h
@@ -28,14 +28,14 @@ public:
   void clear();
   bool enqueue(const float start[3], const float target[3], float feed_mm_s, float requested_accel_mm_s2);
   bool plan(float first_entry_speed_mm_s = cfg::MIN_PROFILE_SPEED_MM_S);
-  bool seedPrepared(const PathMove &move);
+  bool popFront(PathMove *out = nullptr);
 
   uint8_t count() const { return count_; }
   bool empty() const { return count_ == 0; }
   bool full() const { return count_ >= cfg::PATH_QUEUE_SIZE; }
 
-  const PathMove &move(uint8_t index) const { return moves_[index]; }
-  PathMove &move(uint8_t index) { return moves_[index]; }
+  const PathMove &move(uint8_t index) const { return moves_[physicalIndex(index)]; }
+  PathMove &move(uint8_t index) { return moves_[physicalIndex(index)]; }
   void latestTarget(float xyz[3]) const;
 
   static float junctionSpeed(const PathMove &prev, const PathMove &next);
@@ -43,8 +43,12 @@ public:
 private:
   Kinematics &kinematics_;
   PathMove moves_[cfg::PATH_QUEUE_SIZE];
+  uint8_t head_;
   uint8_t count_;
 
+  uint8_t physicalIndex(uint8_t logical) const {
+    return uint8_t((uint16_t(head_) + logical) % cfg::PATH_QUEUE_SIZE);
+  }
   bool prepareMove(PathMove &m, const float start[3], const float target[3],
                    float feed_mm_s, float requested_accel_mm_s2);
 };
