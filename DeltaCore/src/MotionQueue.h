5347d22e3839bf356162453e54bff9b95b429261
Halawania <abdu.halwani@gmail.com>
2026-08-29 18:38:23 +0300

    Carry smoothing level in motor blocks

--- DeltaCore/src/MotionQueue.h
+++ DeltaCore/src/MotionQueue.h
@@ -7,33 +7,28 @@ namespace deltacore {
 
 struct MotorBlock {
   uint32_t steps[3];
-  uint8_t direction_bits;   // bit set = positive tower motion
-  uint16_t interval_ticks;  // constant DDA event interval for this short segment
+  uint8_t direction_bits;
+  uint8_t smoothing_level;
+  uint16_t interval_ticks;
 };
 
 class MotionQueue {
 public:
   MotionQueue();
-
   bool enqueue(const MotorBlock &block);
   bool popFromISR(MotorBlock &block);
   bool empty() const;
   bool full() const;
   uint8_t count() const;
   uint8_t freeSlots() const;
-
   void clear();
-  void clearUnsafe(); // caller must already have interrupts disabled
+  void clearUnsafe();
 
 private:
   MotorBlock buffer_[cfg::MOTION_QUEUE_SIZE];
   volatile uint8_t head_;
   volatile uint8_t tail_;
-
-  static uint8_t nextIndex(uint8_t i) {
-    ++i;
-    return i >= cfg::MOTION_QUEUE_SIZE ? 0 : i;
-  }
+  static uint8_t nextIndex(uint8_t i) { ++i; return i >= cfg::MOTION_QUEUE_SIZE ? 0 : i; }
 };
 
 } // namespace deltacore
