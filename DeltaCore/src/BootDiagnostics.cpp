9224cc3b7103f0c8fc5f6b7784824cf5a3e46e34
Halawania <abdu.halwani@gmail.com>
2026-08-29 19:37:46 +0300

    Track persistent boot session across reconnects

--- DeltaCore/src/BootDiagnostics.cpp
+++ DeltaCore/src/BootDiagnostics.cpp
@@ -1,9 +1,12 @@
 #include "BootDiagnostics.h"
 
+#include <avr/eeprom.h>
 #include <avr/io.h>
 #include <avr/wdt.h>
 
 static uint8_t g_reset_cause __attribute__((section(".noinit")));
+static uint32_t g_boot_session = 0;
+static uint32_t EEMEM ee_boot_counter;
 
 void captureResetCauseEarly() __attribute__((naked, section(".init3"), used));
 void captureResetCauseEarly() {
@@ -13,5 +16,18 @@ void captureResetCauseEarly() {
 }
 
 namespace deltacore {
+
 uint8_t bootResetCause() { return g_reset_cause; }
+
+uint32_t beginBootSession() {
+  uint32_t value = eeprom_read_dword(&ee_boot_counter);
+  if (value == 0xFFFFFFFFUL || value == 0UL) value = 1UL;
+  else ++value;
+  eeprom_update_dword(&ee_boot_counter, value);
+  g_boot_session = value;
+  return g_boot_session;
 }
+
+uint32_t bootSessionId() { return g_boot_session; }
+
+} // namespace deltacore
