4f52cccd3ca723d6f8ec579ace221e2a942ad9dd
Halawania <abdu.halwani@gmail.com>
2026-08-29 18:39:30 +0300

    Add v0.3 path batching and adaptive segment generator

--- DeltaCore/src/MotionController.h
+++ DeltaCore/src/MotionController.h
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include "Kinematics.h"
 #include "MotionQueue.h"
+#include "PathPlanner.h"
 #include "StepperEngine.h"
 
 namespace deltacore {
@@ -13,6 +14,7 @@ enum RequestResult : uint8_t {
   REQUEST_NOT_HOMED,
   REQUEST_OUT_OF_BOUNDS,
   REQUEST_KINEMATICS,
+  REQUEST_QUEUE_FULL,
   REQUEST_FAULT,
   REQUEST_INVALID
 };
@@ -26,27 +28,25 @@ enum ControllerEvent : uint8_t {
 
 class MotionController {
 public:
-  MotionController(MotionQueue &queue, StepperEngine &stepper, Kinematics &kinematics);
-
+  MotionController(MotionQueue &queue, StepperEngine &stepper, Kinematics &kinematics, PathPlanner &planner);
   void begin();
   void service();
-
   RequestResult requestHome();
   RequestResult requestMove(const float target_xyz[3], float feed_mm_s);
+  void flushMoves();
   void emergencyStop();
   bool clearFault();
-  void invalidatePosition() { homed_ = false; }
-
+  void invalidatePosition() { homed_ = false; planner_.clear(); }
   bool homed() const { return homed_; }
   bool busy() const;
-  bool moving() const { return move_active_; }
+  bool moving() const { return batch_active_; }
   bool homing() const { return home_state_ != HOME_IDLE; }
-
+  uint8_t queuedMoves() const { return planner_.count(); }
   void currentPosition(float xyz[3]) const;
+  void commandPosition(float xyz[3]) const;
   float feedrate() const { return default_feed_mm_s_; }
   float acceleration() const { return acceleration_mm_s2_; }
   bool setAcceleration(float mm_s2);
-
   ControllerEvent consumeEvent();
   RequestResult lastRequestError() const { return last_request_error_; }
 
@@ -56,32 +56,36 @@ private:
   MotionQueue &queue_;
   StepperEngine &stepper_;
   Kinematics &kinematics_;
+  PathPlanner &planner_;
 
   bool homed_;
   HomeState home_state_;
   volatile ControllerEvent event_;
   RequestResult last_request_error_;
-
   float current_xyz_[3];
   int32_t home_motor_steps_[3];
 
-  bool move_active_;
+  bool batch_active_;
   bool generation_complete_;
-  float move_start_[3];
-  float move_target_[3];
-  float move_length_mm_;
-  float move_feed_mm_s_;
-  float acceleration_mm_s2_;
-  float default_feed_mm_s_;
-  float profile_peak_mm_s_;
-  float profile_accel_distance_mm_;
-  uint32_t total_segments_;
-  uint32_t generated_segments_;
+  bool flush_requested_;
+  uint32_t last_enqueue_ms_;
+  uint8_t generating_index_;
+  float generated_distance_mm_;
   int32_t generated_motor_steps_[3];
   int32_t final_motor_steps_[3];
+  float profile_peak_mm_s_;
+  float profile_accel_distance_mm_;
+  float profile_decel_distance_mm_;
 
+  float acceleration_mm_s2_;
+  float default_feed_mm_s_;
+
+  bool startBatch();
+  bool initGeneratingMove(uint8_t index);
   bool generateOneSegment();
-  float profileSpeed(float distance_mm) const;
+  float profileSpeed(const PathMove &move, float distance_mm) const;
+  float adaptiveSegmentLength(const PathMove &move, float distance_mm) const;
+  uint8_t smoothingLevelForTicks(uint32_t base_ticks) const;
   static float smootherStep5(float u);
   bool validatePath(const float start[3], const float target[3]) const;
   bool towerWithinHome(const int32_t tower_steps[3]) const;
