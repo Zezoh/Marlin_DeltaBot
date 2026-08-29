395ec2b41a1328aa9138a7cc75669a4cbb64876c
Halawania <abdu.halwani@gmail.com>
2026-08-29 18:37:42 +0300

    Add Delta tower motion metrics

--- DeltaCore/src/Kinematics.h
+++ DeltaCore/src/Kinematics.h
@@ -4,6 +4,11 @@
 
 namespace deltacore {
 
+struct MotionMetrics {
+  float max_gain;
+  float max_curvature;
+};
+
 class Kinematics {
 public:
   Kinematics();
@@ -11,6 +16,9 @@ public:
   bool cartesianToTower(const float xyz[3], float tower_mm[3]) const;
   bool cartesianToSteps(const float xyz[3], int32_t tower_steps[3]) const;
   bool withinSoftBounds(const float xyz[3]) const;
+  bool motionMetrics(const float start[3], const float unit[3], float length_mm,
+                     MotionMetrics &metrics) const;
+  bool towerChordError(const float p0[3], const float p1[3], float &error_mm) const;
 
 private:
   float tower_xy_[3][2];
